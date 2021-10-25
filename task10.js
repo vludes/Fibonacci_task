1ba8d8274f0d16fa4943aaae6c9bfe9a2aeda2e1
Vladyslav Lototskyy <lototskyi.vladislav@gmail.com>
2021-10-25 19:48:25 +0300

    Used the reduce method

--- task10.js
+++ task10.js
@@ -1,47 +1,42 @@
 const button = document.querySelector("#submit");
 
-
-
 // Creating click event for the button to call the func
-button.addEventListener('click', function() {
-    
-  //  The entered value is splitted by commas into an array
-  const arrVal = document.querySelector(".fibonacciInput").value.split(",");
+button.addEventListener('click', function () {
 
+  //  The entered value is splitted by commas into an array
+  var arrVal = document.querySelector(".fibonacciInput").value.split(",");
+  const toNumbers = arr => arr.map(Number);
+  arrVal = toNumbers(arrVal);
 
   // Checks if the entered value is not empty
-  if(arrVal == "") {
+  if (arrVal == "") {
     // Adding a warning
     document.querySelector(".input").innerHTML = "Please enter your value";
     document.querySelector(".output").innerHTML = "";
   } else {
     // Calling the Fibonacci function
     const result = arrChallenge(arrVal);
-    document.querySelector(".input").innerHTML = "Input: " + arrVal;  
-    document.querySelector(".output").innerHTML = "Output: " + result;  
+    document.querySelector(".input").innerHTML = "Input: " + arrVal;
+    document.querySelector(".output").innerHTML = "Output: " + result;
   }
 });
 
-function arrChallenge (arr) {
-    var sumArray = 0;
-    // Obtaining the sum of the array
-    for(i = 0; i < arr.length; i++) {
-      // String converted to numbers
-      sumArray += parseInt(arr[i]);
-  }
-
+function arrChallenge(arr) {
+  var sumArray = 0;
+  // Obtaining the sum of the array
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+  sumArray = arr.reduce(reducer);
 
   // Finding Fibonacci numbers
-  var a = 0;
-	var b = 1;
-	var c = a + b;
-	while (c < sumArray)
-	{
-		a = b;
-		b = c;
-		c = a + c;
-	}
-	
-    // Returning the needed value
-    return c - sumArray;
+  var firstPrevious = 0;
+  var secondPrevious = 1;
+  var fibonacciNumber = firstPrevious + secondPrevious;
+  while (fibonacciNumber < sumArray) {
+    firstPrevious = secondPrevious;
+    secondPrevious = fibonacciNumber;
+    fibonacciNumber = firstPrevious + fibonacciNumber;
+  }
+
+  // Returning the needed value
+  return fibonacciNumber - sumArray;
 }
